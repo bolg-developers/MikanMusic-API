96bc2463c698657f169b0e7257baf3acd71da988
KokiMatsumura <matsumura.koki1@gmail.com>
2019-06-03 00:21:08 +0900

    add: ダミーデータのsql

--- migration/20190522161444_create_users.up.sql
+++ migration/20190522161444_create_users.up.sql
@@ -8,3 +8,6 @@ CREATE TABLE IF NOT EXISTS users (
         created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
         updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+INSERT INTO users (id, display_name, icon_url, introduction, email, password, created_at, updated_at)
+        VALUES('f2526355-03ba-472b-ab29-2688951d039f', '本田翼', 'https://s3-ap-northeast-1.amazonaws.com/mikanmusic/01f9e044-a3bd-46b5-a586-3ccfdfd01aad.jpg', 'ゲームすきです。', 'ilovegame@gmail.com', '$2a$10$I54htoPl2vx7Q1q0RLubAOffUbamgK3mvzKAPS7MbhsF41YjNGV6i', '2019-05-31 00:19:17', '2019-05-31 00:19:17');
